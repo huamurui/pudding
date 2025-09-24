864406d6e7c0fa81fe4269e403eb28b900e15d3d
huamurui <huamurui@outlook.com>
2025-09-24 19:40:46 +0800

    fix date

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -30,7 +30,7 @@ const PostList = ({ posts, selectedTags = () => null }: PostListProps) => {
                   () => {
                     const date = new Date(post.date);
                     const year = date.getFullYear();
-                    const month = String(date.getMonth()).padStart(2, "0");
+                    const month = String(date.getMonth()+1).padStart(2, "0");
                     const day = String(date.getDate()).padStart(2, "0");
                     return `${year}-${month}-${day}`;
                   }
